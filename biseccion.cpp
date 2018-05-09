4097305fd1ce2d174b7bea872e70751c59045f4b
Kur <deadkuriel@gmail.com>
2018-05-09 10:27:19 -0500

    Copia
    Copia de los archivos que ya se tenian.

---
+++ biseccion.cpp
@@ -0,0 +1,13 @@
+#include "biseccion.h"
+#include <iostream>
+
+using namespace std;
+
+Biseccion::Biseccion(int x, int y)
+{
+    cout<<"Aqui va el programa de Biseccion";
+}
+
+Biseccion::~Biseccion()
+{
+}
---
+++ biseccion.h
@@ -0,0 +1,14 @@
+#ifndef BISECCION_H
+#define BISECCION_H
+
+class Biseccion
+{
+      int x, y;
+	public:
+		// class constructor
+		Biseccion(int x, int y);
+		// class destructor
+		~Biseccion();
+};
+
+#endif // BISECCION_H
