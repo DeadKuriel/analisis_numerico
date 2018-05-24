b5d4c6d8f88569b28f33f1aa2b8bc98480bf696d
Kur <deadkuriel@gmail.com>
2018-05-24 13:50:40 -0500

    Cree calculo.h
    calculo.h nos servirá para escribir en el, los cálculos necesarios que se repitan mucho.

---
+++ calculo.h
@@ -0,0 +1,17 @@
+#ifndef CALCULO_H
+#define CALCULO_H
+ 
+class Calculo
+{
+    private:
+        int x;
+ 
+	public:
+		// class constructor
+		(void);
+
+		// class destructor
+		~Calculo();
+};
+ 
+#endif // CALCULO_H
